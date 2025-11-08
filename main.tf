8bf3f2c660dc09068f1e67a1c1f1687041ddca49
iBrid <col.buche02@gmail.com>
2025-11-08 15:24:50 -0600

    updating token to classic toekn in GitHub

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 
-#create a subnet within the virtual network with the name "StaticWebAppSubnet" and address prefix "
+#create a subnet within the virtual network with the name "StaticWebAppSubnet" and address prefix
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
