7d0f376623b3fb04c8c0b4e2d2fb255bd62d6ad8
iBrid <col.buche02@gmail.com>
2025-11-08 15:12:49 -0600

    Updating location for Static Web App to West US 2 as West US 3 not supported.

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "subnet" {
 #add the static web app resource
 resource "azurerm_static_web_app" "static_web_app" {
   name                = var.static_web_app_name
-  location            = azurerm_resource_group.rg.location
+  location            = "West Us 2"
   resource_group_name = azurerm_resource_group.rg.name
   sku_tier            = "Free"
   repository_url      = "https://github.com/iBrid/my_webpage.git"
